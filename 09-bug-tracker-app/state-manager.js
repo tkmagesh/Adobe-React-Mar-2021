e7c7d1a410c8cf1b4444e38615a22f3b1b527e29
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-25 20:53:18 +0530

    bug tracker 1 - initial

---
+++ 09-bug-tracker-app/state-manager.js
@@ -0,0 +1,48 @@
+let StateManager = (function(){
+    let _currentState = undefined,
+        _subscriptionFns = [],
+        _reducer = undefined,
+        _init_action = { type : '@@INIT-ACTION' };
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(subscriptionFn){
+        _subscriptionFns.push(subscriptionFn);
+    }
+
+    function notifyChanges(){
+        _subscriptionFns.forEach(subscriptionFn => subscriptionFn());
+    }
+
+    function dispatch(action){
+        let newState = _reducer(_currentState, action);
+        if (newState === _currentState) return;
+        _currentState = newState;
+        notifyChanges();
+    }
+
+    function createStore(reducer){
+        if (typeof reducer !== 'function')
+            throw new Error('A reducer is mandatory to create the store!');
+        _reducer = reducer;
+        //initializing the currentState with a valid default state
+        _currentState = _reducer(_currentState, _init_action);
+        let store = { getState, subscribe, dispatch };
+        return store;
+    }
+
+    function bindActionCreators(actionCreators, dispatch){
+        const actionDispatchers = {};
+        for(let key in actionCreators){
+            actionDispatchers[key] = function(...args){
+                const action = actionCreators[key](...args);
+                dispatch(action);
+            }
+        }
+        return actionDispatchers;
+    }
+
+    return { createStore, bindActionCreators }
+})()
