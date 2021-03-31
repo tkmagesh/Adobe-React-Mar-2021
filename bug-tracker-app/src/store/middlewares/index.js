4c64f47c5c1f7583db0337597bc0a0ec99ddefee
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-31 22:58:49 +0530

    using redux-logger & redux-thunk

--- bug-tracker-app/src/store/middlewares/index.js
+++ bug-tracker-app/src/store/middlewares/index.js
@@ -1,8 +1,22 @@
 import { applyMiddleware } from 'redux';
 
-import loggerMiddleware from './loggerMiddleware';
+/* import loggerMiddleware from './loggerMiddleware';
 import asyncMiddleware from './asyncMiddleware';
+import promiseMiddleware from './promiseMiddleware';
 
-const appMiddlewares = applyMiddleware(loggerMiddleware, asyncMiddleware);
+const appMiddlewares = applyMiddleware(loggerMiddleware, asyncMiddleware, promiseMiddleware);
+ */
+
+/* import logger from 'redux-logger';
+import asyncMiddleware from './asyncMiddleware';
+import promiseMiddleware from './promiseMiddleware';
+
+const appMiddlewares = applyMiddleware(logger, asyncMiddleware, promiseMiddleware); */
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promiseMiddleware';
+
+const appMiddlewares = applyMiddleware(logger, thunk, promiseMiddleware);
 
 export default appMiddlewares;
---
+++ bug-tracker-app/src/store/middlewares/promiseMiddleware.js
@@ -0,0 +1,9 @@
+const promiseMiddleware = store => next => action => {
+    if (action instanceof Promise){
+        action.then(actionObj => store.dispatch(actionObj))
+    } else {
+        return next(action);
+    }
+}
+
+export default promiseMiddleware;
